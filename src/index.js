a378135556b566aeccaf2ae30d115b5b1b969e78
Roxana <roxanna.sa@gmail.com>
2023-07-13 18:31:51 -0400

    Initial config ESlint, babel & Jest

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { constante } from './util.js';
+import * as fs from 'fs';
+
+//jdasdha
+// export function largoTarjetaCredito(evento) {
+//   const creditCardNumber = document.querySelector("#inputNumero").value;
+//   const largoString = creditCardNumber.length;
+//   if (largoString > 16) {
+//     evento.preventDefault(); // previene que se escriban mas de 16 caracteres
+//   }
+// }
+
+console.log(fs.readdirSync('./'));
+
+console.log(constante);
