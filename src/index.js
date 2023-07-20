9f53b719addf144687f3ed0323e431f16c25c574
Roxana <roxanna.sa@gmail.com>
2023-07-20 13:25:19 -0400

    Refactoring mdLinks

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import chalk from "chalk";
 import { routeVerification,
-convertToAbsoluteRoute,
-isFileMd,
-isADirectory,
-hasLinks,
-processFilesRecursively
+getMDFileRoutes,
+getLinksAndValidate,
+areLinksRemaining
 } from "./functions.js";
 
-
+const path = '../md files/';
+const options = { validate: true };
 
 function mdlinks(path, options) {
   return new Promise((resolve, reject) => {
     const routes = [];
     const links = [];
-
+    if (options == null){
+      options = {
+        validate: false,
+        stats: false
+      }
+    }
     //verificar que la variable path no sea null
     if (path === null || path === undefined || path === '') {
       console.log(chalk.bgRed.bold('Error: There is no route'));
       reject('Error: There is no route');
       return;
     }
-
     //verificar que la ruta existe
     const valid = routeVerification(path);
     if (!valid) {
@@ -28,58 +31,36 @@ function mdlinks(path, options) {
       reject('Error: Path does not exist');
       return;
     }
+    // Obtener todos los archivos md en la ruta y agregarlos a routes
+    getMDFileRoutes(path, routes);
 
-    //verificar si ruta es absoluta, en caso de que no, transformar.
-    const absolutePath = convertToAbsoluteRoute(path);
-    console.log(`Absolute route: ${absolutePath}`); //*borrar a futuro
-
-    const isDirectoryResult = isADirectory(absolutePath);
-    console.log(chalk.blue.underline.bold(`¿Is a directory?: ${isDirectoryResult}`)); //*borrar a futuro
-
-    if (isDirectoryResult) {
-      processFilesRecursively(routes,absolutePath); 
-    } else {
-      // Si es un archivo...
-      const isAFileMd = isFileMd(absolutePath);
-      console.log(`¿Is it a .md?: ${isAFileMd}`);
-      if (!isAFileMd) {
-        console.log('It is not a .md file');
-        reject('It is not a .md file');
-        return;
-      }
-
-      routes.push(absolutePath);
-    }
-
-    // ¿Existen archivos .md por procesar?
-    if (routes.length === 0){
-      console.log('"Error: There are no .md files');
-      reject ('"Error: There are no .md files');
-    }
-
-    // Procesar el próximo archivo del listado
-    for (const currentMdFile of routes){
-      // Add function to add all links to the array with the following code:
-
-      console.log(currentMdFile)
-      hasLinks(currentMdFile)
-      .then(isThereALink => {
-        console.log(`isThereALink in ${currentMdFile}?:`, isThereALink);
-        if (!isThereALink){
-          return; // Termina la ejecución del .then
-        }
-
-        console.log("si había links, continuar")
+    getLinksAndValidate(routes, options)
+      .then(links => {
+        resolve(links);
       })
-    }
-
-    resolve(routes);
+      .catch(error => {
+        console.log('Error during link processing:', error);
+        reject(error);
+      });
   });
 }
 
+  
+
+// Obtener las rutas antes de llamar a mdlinks
+const routes = [];
+
+// Llamamos a mdlinks con la opción validate adecuada
+mdlinks(path, options)
+  .then(() => console.log(areLinksRemaining([], [], routes))
+    ? "Aún quedan enlaces por procesar."
+    : "Todos los enlaces han sido procesados.")
+  .then(message => console.log(message))
+  .catch(error => console.log('error on call - ', error));
 
 //mdlinks();
-mdlinks('../md files')
-.then(res => console.log(res))
-.catch(error => console.log('error', error));
+/*mdlinks('../md files/', { validate: true })
+  .then(res => console.log("final sin log c:"))
+  .catch(error => console.log('error on call - ', error));*/
+
 
