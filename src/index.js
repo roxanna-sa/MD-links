be2b0d224ed4a939087184408793f347bb5ee5c8
Roxana <roxanna.sa@gmail.com>
2023-07-25 20:46:51 -0400

    Deleting comments

--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
-import chalk from "chalk";
 import { routeVerification,
 getMDFileRoutes,
-getLinksAndValidate,
-calculateStats
+getLinksAndValidate
 } from "./functions.js";
 
 
 export function mdlinks(path, options) {
   return new Promise((resolve, reject) => {
     const routes = [];
-    /*const links = [];
-    if (options == null){
-      options = {
-        validate: false,
-        stats: false
-      }
-    }*/
     //verificar que la variable path no sea null
     if (path === null || path === undefined || path === '') {
-      console.log(chalk.bgRed.bold('Error: There is no route'));
+      //console.log(chalk.bgRed.bold('Error: There is no route'));
       reject('Error: There is no route');
       return;
     }
     //verificar que la ruta existe
     const valid = routeVerification(path);
     if (!valid) {
-      console.log(chalk.red('Error: Path does not exist'));
+      //console.log(chalk.red('Error: Path does not exist'));
       reject('Error: Path does not exist');
       return;
     }
@@ -34,27 +25,15 @@ export function mdlinks(path, options) {
 
     getLinksAndValidate(routes, options)
       .then(links => {
-
-        calculateStats(links, options);
-
         resolve(links);
       })
       .catch(error => {
-        console.log('Error during link processing:', error);
-        reject(error);
+        reject('Error during link processing:', error);
       });
   });
 }
 
-/*const path = '../md files/';
-const options = { 
-  validate: true,
-  stats: true
-};
-// Llamamos a mdlinks con la opción validate 
-mdlinks(path, options)
-  .then(message => console.log(message))
-  .catch(error => console.log('Call status - ', error));*/
+
 
 
 
