91995e5b1884ce49f726faf4a4233d478f100154
Roxana <roxanna.sa@gmail.com>
2023-07-25 20:48:13 -0400

    Change in banner & minor changes

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 import figlet from "figlet";
+import chalk from "chalk";
 import { mdlinks } from "./index.js";
+import { calculateStats } from "./functions.js";
 
 
 // Función para mostrar el mensaje de MDlinks en ASCII art
 const showBanner = () => {
   const bannerText = figlet.textSync("Md-Links", {
-    font: "Doom",
+    font: "big",
     horizontalLayout: "default",
     verticalLayout: "default",
   });
-  console.log(bannerText);
+  const bannerConColor = chalk.green.bold(bannerText);
+
+  console.log(bannerConColor);
 };
 
 // Mostrar el mensaje de MDlinks al iniciar el CLI
@@ -27,20 +31,25 @@ const options = {
 
 mdlinks(path, options)
   .then((links) => {
-    links.forEach((link) => {
-      const { href, text, file, status, ok } = link;
-      if (options.stats && options.validate) {
-        console.log(`${file} ${href} ${ok} ${status} ${text}`);
-      } else if (options.stats) {
-        console.log(`${file} ${href} ${text}`);
-      } else if (options.validate) {
-        console.log(`${file} ${href} ${ok} ${text}`);
-      } else {
-        console.log(`${file} ${href} ${text}`);
+    // Imprimimos por pantalla el resultado del proceso
+    links.forEach(validation => {
+      console.log(`href: ${validation.href}`);
+      console.log(`text: ${validation.text}`);
+      console.log(`file: ${validation.file}`);
+      if (options.validate){
+        console.log(`status: ${validation.status}`);
+        if(validation.ok === 'ok'){
+          console.log(chalk.green.bold(`ok: ${validation.ok}`));
+        }else{
+          console.log(chalk.red.bold(`ok: ${validation.ok}`));
+        }
+      
       }
+      console.log('--------------------------');
     });
-    /*if (options.stats) {
+
+    if(options.stats){
       calculateStats(links, options);
-    }*/
+    }
   })
-  .catch((error) => console.log('Error:', error));
+  .catch((error) => console.log(chalk.red(error)));
